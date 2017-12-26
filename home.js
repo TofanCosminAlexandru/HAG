40bdf59399aa8bc83645072cdd0e34c96c619310
Tofan Cosmin-Alexandru <tofan_cosmin@yahoo.com>
2017-12-26 23:51:48 +0200

    a few changes

--- home.js
+++ home.js
@@ -2,7 +2,7 @@ var displayedImage = document.querySelector('.displayed-img'); // imaginea princ
 var secondaryImages = document.querySelector('.secondary-images'); // bara cu imaginile secundare
 
 secondaryImages.onclick = function(e) {
-	if(e.target && e.target.nodeName == 'IMG') { // daca element din div este apasat si acel element este img
+	if(e.target && e.target.nodeName === 'IMG') { // daca element din div este apasat si acel element este img
 		displayedImage.setAttribute('src', e.target.getAttribute('src'));
 	}
 }
--- orizontalNav.js
+++ orizontalNav.js
@@ -2,8 +2,6 @@
 var box = document.getElementById("box");
 window.addEventListener('mousemove', function(e){
 	if(e.pageX > 120 && e.pageX < 800){
-		var left = (e.pageX-700)+"px";
-		box.style.right = left;
+        box.style.right = (e.pageX - 700) + "px";
 	}
 });
-

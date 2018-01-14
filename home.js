61c55b4dcc5285f2f76bbcc657817823e44832c1
cipriann23 <nemtocciprian10@gmail.com>
2018-01-14 22:41:12 +0200

    Firebase nenorocit

--- home.js
+++ home.js
@@ -34,30 +34,86 @@ function register(){
 	    check.style.visibility = "hidden";
 	    logo[0].style.visibility = "visible";
 	}
-
 	el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
 	logo[0].style.visibility = (logo[0].style.visibility === "hidden") ? "visible" : "hidden";
 }
 
-function check_login() {
-	var data = '{ "users" : [' +
-				'{ "username":"John" , "password":"Doe" },' +
-				'{ "username":"Anna" , "password":"Smith" },' +
-				'{ "username":"Peter" , "password":"Jones" } ]}';
+function initialize(value){
 
-	var obj = JSON.parse(data);
-	var username = document.getElementById("login-user").value;
-	var pass = document.getElementById("login-pass").value;
-	check = false;
-	var adapt = document.getElementsByClassName("login")[0];
-	for(var i=0; i<obj.users.length; i++){
-		if(username === obj.users[i].username && obj.users[i].password === pass){
-			adapt.setAttribute('action','levels.html?user=' + username);
-			alert(adapt.action);
-			var check = true;		
+	var config = {
+	apiKey: "AIzaSyCfnDkca7HKVGLekgckbVZWXaEZY-cRbqk",
+	authDomain: "hag-game.firebaseapp.com",
+	databaseURL: "https://hag-game.firebaseio.com",
+	projectId: "hag-game",
+	storageBucket: "hag-game.appspot.com",
+	messagingSenderId: "791209373333"
+	};
+
+	firebase.initializeApp(config);		
+
+}
+
+function writeUserData(userId, name, password, lvl, nr_stars, nr_gems) {
+
+  firebase.database().ref('users/' + userId).set({
+  	ID: userId,
+    username: name,
+    pass: password,
+    level: lvl,
+    stars: nr_stars,
+    gems: nr_gems
+  });
+}
+
+function check_login() {
+	var data;
+
+	//var adapt = document.getElementsByClassName("login")[0];
+	//adapt.setAttribute('action','levels.html?user=fdasfads');
+
+	firebase.database().ref().once('value').then((snapshot) => {
+		var data = snapshot.val();
+		console.log(data);
+		var log_username = document.getElementById("login-user").value;
+		var log_pass = document.getElementById("login-pass").value;
+		var check = false;
+		//var adapt = document.getElementsByClassName("login")[0];
+		
+		for(var i=1; i<data.users.length; i++){
+			if(log_username == data.users[i].username && data.users[i].pass == log_pass){
+				//adapt.setAttribute('action','levels.html?user=' + log_username);
+				var check = true;		
+			}
+
 		}
-	}
-	if(check === false){
-		alert("Wrong Username or Password");
+		if(check == false){
+			alert("Wrong Username or Password");
+		}else{
+			window.location = "levels.html?user="+log_username;
+		}
+	});
+}
+
+var data;
+
+function register_new(){
+	var username = document.getElementById("register-user").value;
+	var pass = document.getElementById("register-pass").value;
+	var confirmpass = document.getElementById("register-confirmpass").value;
+	var data;
+	//writeUserData(1,"Ana","Mere",0,0,0);
+	if(pass != confirmpass){
+		alert("Different passwords!");
+	}else{
+				
+		firebase.database().ref().once('value').then((snapshot) => {
+		  this.data = snapshot.val();
+		  writeUserData(this.data.users.length,username,pass,0,0,0);
+		  //console.log(this.data)
+		});
+		//console.log(this.data);		
+		alert("Registration successful HAPPY new LOGIN");
+		login();
 	}
 }
+

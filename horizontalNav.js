4c7cf015809de87979089e3d22c9ddffbb1449da
Tofan Cosmin-Alexandru <tofan_cosmin@yahoo.com>
2018-01-14 19:45:13 +0200

    replace all file

--- horizontalNav.js
+++
@@ -1,8 +0,0 @@
-// var a = window.document.defaultView.getComputedStyle(document.getElementsByClassName("level ground")[0], null).getPropertyValue('background-image').split(/['"]/)[1];
-
-var box = document.getElementById("box");
-window.addEventListener('mousemove', function(e){
-	if(e.pageX > 120 && e.pageX < 800){
-        box.style.right = (e.pageX - 700) + "px";
-	}
-});

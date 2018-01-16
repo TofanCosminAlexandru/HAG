0e3dbbc740aa89720348ddc00e3a0f5e0cb9e1bd
Tofan Cosmin-Alexandru <tofan_cosmin@yahoo.com>
2018-01-16 12:01:13 +0200

    offline mode

--- home.js
+++ home.js
@@ -59,82 +59,91 @@ function register(){
 	logo[0].style.visibility = (logo[0].style.visibility === "hidden") ? "visible" : "hidden";
 }
 
-function initialize(value){
+console.log(navigator.onLine);
 
-	var config = {
-	apiKey: "AIzaSyCfnDkca7HKVGLekgckbVZWXaEZY-cRbqk",
-	authDomain: "hag-game.firebaseapp.com",
-	databaseURL: "https://hag-game.firebaseio.com",
-	projectId: "hag-game",
-	storageBucket: "hag-game.appspot.com",
-	messagingSenderId: "791209373333"
-	};
+if(navigator.onLine) {
 
-	firebase.initializeApp(config);
+    function initialize(value) {
 
+        var config = {
+            apiKey: "AIzaSyCfnDkca7HKVGLekgckbVZWXaEZY-cRbqk",
+            authDomain: "hag-game.firebaseapp.com",
+            databaseURL: "https://hag-game.firebaseio.com",
+            projectId: "hag-game",
+            storageBucket: "hag-game.appspot.com",
+            messagingSenderId: "791209373333"
+        };
+
+        firebase.initializeApp(config);
+
+    }
+
+    function writeUserData(userId, name, password, lvl, nr_stars, nr_gems) {
+
+        firebase.database().ref('users/' + userId).set({
+            ID: userId,
+            username: name,
+            pass: password,
+            level: lvl,
+            stars: nr_stars,
+            gems: nr_gems
+        });
+    }
+
+    function check_login() {
+        var data;
+
+        //var adapt = document.getElementsByClassName("login")[0];
+        //adapt.setAttribute('action','levels.html?user=fdasfads');
+
+        firebase.database().ref().once('value').then((snapshot) => {
+            var data = snapshot.val();
+            console.log(data);
+            var log_username = document.getElementById("login-user").value;
+            var log_pass = document.getElementById("login-pass").value;
+            var check = false;
+            //var adapt = document.getElementsByClassName("login")[0];
+
+            for (var i = 1; i < data.users.length; i++) {
+                if (log_username == data.users[i].username && data.users[i].pass == log_pass) {
+                    //adapt.setAttribute('action','levels.html?user=' + log_username);
+                    var check = true;
+                }
+
+            }
+            if (check == false) {
+                alert("Wrong Username or Password");
+            } else {
+                window.location = "levels.html?user=" + log_username;
+            }
+        });
+    }
+
+    var data;
+
+    function register_new() {
+        var username = document.getElementById("register-user").value;
+        var pass = document.getElementById("register-pass").value;
+        var confirmpass = document.getElementById("register-confirmpass").value;
+        var data;
+        //writeUserData(1,"Ana","Mere",0,0,0);
+        if (pass != confirmpass) {
+            alert("Different passwords!");
+        } else {
+
+            firebase.database().ref().once('value').then((snapshot) => {
+                this.data = snapshot.val();
+                writeUserData(this.data.users.length, username, pass, 0, 0, 0);
+                //console.log(this.data)
+            });
+            //console.log(this.data);
+            alert("Registration successful HAPPY new LOGIN");
+            login();
+        }
+    }
 }
-
-function writeUserData(userId, name, password, lvl, nr_stars, nr_gems) {
-
-  firebase.database().ref('users/' + userId).set({
-  	ID: userId,
-    username: name,
-    pass: password,
-    level: lvl,
-    stars: nr_stars,
-    gems: nr_gems
-  });
+else {
+    document.getElementById("loginButton").addEventListener("click", function () {
+        location.href = "levels.html";
+    });
 }
-
-function check_login() {
-	var data;
-
-	//var adapt = document.getElementsByClassName("login")[0];
-	//adapt.setAttribute('action','levels.html?user=fdasfads');
-
-	firebase.database().ref().once('value').then((snapshot) => {
-		var data = snapshot.val();
-		console.log(data);
-		var log_username = document.getElementById("login-user").value;
-		var log_pass = document.getElementById("login-pass").value;
-		var check = false;
-		//var adapt = document.getElementsByClassName("login")[0];
-		
-		for(var i=1; i<data.users.length; i++){
-			if(log_username == data.users[i].username && data.users[i].pass == log_pass){
-				//adapt.setAttribute('action','levels.html?user=' + log_username);
-				var check = true;
-			}
-
-		}
-		if(check == false){
-			alert("Wrong Username or Password");
-		}else{
-			window.location = "levels.html?user="+log_username;
-		}
-	});
-}
-
-var data;
-
-function register_new(){
-	var username = document.getElementById("register-user").value;
-	var pass = document.getElementById("register-pass").value;
-	var confirmpass = document.getElementById("register-confirmpass").value;
-	var data;
-	//writeUserData(1,"Ana","Mere",0,0,0);
-	if(pass != confirmpass){
-		alert("Different passwords!");
-	}else{
-
-		firebase.database().ref().once('value').then((snapshot) => {
-		  this.data = snapshot.val();
-		  writeUserData(this.data.users.length,username,pass,0,0,0);
-		  //console.log(this.data)
-		});
-		//console.log(this.data);
-		alert("Registration successful HAPPY new LOGIN");
-		login();
-	}
-}
-
--- horizontalNav.js
+++ horizontalNav.js
@@ -1,118 +1,170 @@
 // var a = window.document.defaultView.getComputedStyle(document.getElementsByClassName("level ground")[0], null).getPropertyValue('background-image').split(/['"]/)[1];
 
-var box = document.getElementById("box");
-window.addEventListener('mousemove', function(e){
-	if(e.pageX > 120 && e.pageX < 800){
-        box.style.right = (e.pageX - 700) + "px";
-	}
-});
+console.log(navigator.onLine);
 
-function initialize(value){
-    var config = {
-        apiKey: "AIzaSyCfnDkca7HKVGLekgckbVZWXaEZY-cRbqk",
-        authDomain: "hag-game.firebaseapp.com",
-        databaseURL: "https://hag-game.firebaseio.com",
-        projectId: "hag-game",
-        storageBucket: "hag-game.appspot.com",
-        messagingSenderId: "791209373333"
-    };
-    firebase.initializeApp(config);
-}
+if(navigator.onLine) {
+    var box = document.getElementById("box");
+    window.addEventListener('mousemove', function (e) {
+        if (e.pageX > 120 && e.pageX < 800) {
+            box.style.right = (e.pageX - 700) + "px";
+        }
+    });
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
+    function initialize(value) {
+        var config = {
+            apiKey: "AIzaSyCfnDkca7HKVGLekgckbVZWXaEZY-cRbqk",
+            authDomain: "hag-game.firebaseapp.com",
+            databaseURL: "https://hag-game.firebaseio.com",
+            projectId: "hag-game",
+            storageBucket: "hag-game.appspot.com",
+            messagingSenderId: "791209373333"
+        };
+        firebase.initializeApp(config);
+    }
 
-function myacount() {
-    var el = document.getElementById("myacount");
-    el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
-}
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
 
-document.body.addEventListener("keydown", function(e){
-    if(e.keyCode === 27 && document.getElementById("myacount").style.visibility === "visible"){
+    function myacount() {
         var el = document.getElementById("myacount");
         el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
     }
-});
 
-var user = getParameterByName('user');
-document.getElementsByClassName("usernametitle")[0].innerHTML = user;
-
-var levels = {
-    0 : 'level ground',
-    1 : 'level water',
-    2 : 'level cloud'
-};
-
-
-firebase.database().ref().once('value').then((snapshot) => {
-    var data = snapshot.val();
-    var id;
-    var found = 0;
-
-    for(var i=1; i<data.users.length && found === 0; i++){
-        if(data.users[i].username === user){
-            id = data.users[i].ID;
-            found = 1;
+    document.body.addEventListener("keydown", function (e) {
+        if (e.keyCode === 27 && document.getElementById("myacount").style.visibility === "visible") {
+            var el = document.getElementById("myacount");
+            el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
         }
+    });
+
+    var user = getParameterByName('user');
+    document.getElementsByClassName("usernametitle")[0].innerHTML = user;
+
+    var levels = {
+        0: 'level ground',
+        1: 'level water',
+        2: 'level cloud'
+    };
+
+
+    firebase.database().ref().once('value').then((snapshot) => {
+        var data = snapshot.val();
+        var id;
+        var found = 0;
+
+        for (var i = 1; i < data.users.length && found === 0; i++) {
+            if (data.users[i].username === user) {
+                id = data.users[i].ID;
+                found = 1;
+            }
+        }
+        var gems = data.users[id].gems;
+        document.getElementsByClassName("score-diamond")[0].innerHTML = String(gems);
+        document.getElementsByClassName("gems-score")[0].innerHTML = String(gems);
+        var stars = data.users[id].stars;
+        document.getElementsByClassName("score-star")[0].innerHTML = String(stars);
+        document.getElementsByClassName("stars-score")[0].innerHTML = String(stars);
+        level = data.users[id].level;
+
+        var locked = document.getElementsByClassName("level-locked");
+
+        if (level === 0) {
+            locked[level].innerHTML = "";
+            document.getElementsByClassName("level ground")[0].style.filter = "grayscale(" + 0 + "%)";
+
+            document.getElementsByClassName(levels[level])[0].addEventListener("click", function () {
+                location.href = "ground.html?id=" + id + '&user=' + user;
+            });
+        }
+        if (level === 1) {
+            locked[level - 1].innerHTML = "";
+            document.getElementsByClassName("level ground")[0].style.filter = "grayscale(" + 0 + "%)";
+            locked[level].innerHTML = "";
+            document.getElementsByClassName("level water")[0].style.filter = "grayscale(" + 0 + "%)";
+
+            document.getElementsByClassName(levels[level - 1])[0].addEventListener("click", function () {
+                location.href = "ground.html?id=" + id + '&user=' + user;
+            });
+            document.getElementsByClassName(levels[level])[0].addEventListener("click", function () {
+                location.href = "water.html?id=" + id + '&user=' + user;
+            });
+        }
+        if (level === 2) {
+            locked[level - 2].innerHTML = "";
+            document.getElementsByClassName("level ground")[0].style.filter = "grayscale(" + 0 + "%)";
+            locked[level - 1].innerHTML = "";
+            document.getElementsByClassName("level water")[0].style.filter = "grayscale(" + 0 + "%)";
+            locked[level].innerHTML = "";
+            document.getElementsByClassName("level cloud")[0].style.filter = "grayscale(" + 0 + "%)";
+
+            document.getElementsByClassName(levels[level - 2])[0].addEventListener("click", function () {
+                location.href = "ground.html?id=" + id + '&user=' + user;
+            });
+            document.getElementsByClassName(levels[level - 1])[0].addEventListener("click", function () {
+                location.href = "water.html?id=" + id + '&user=' + user;
+            });
+            document.getElementsByClassName(levels[level])[0].addEventListener("click", function () {
+                location.href = "sky.html?id=" + id + '&user=' + user;
+            });
+        }
+        // }
+    });
+}
+else {
+    alert("Offline");
+    var box = document.getElementById("box");
+    window.addEventListener('mousemove', function (e) {
+        if (e.pageX > 120 && e.pageX < 800) {
+            box.style.right = (e.pageX - 700) + "px";
+        }
+    });
+
+    function myacount() {
+        var el = document.getElementById("myacount");
+        el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
     }
-    var gems = data.users[id].gems;
-    document.getElementsByClassName("score-diamond")[0].innerHTML = String(gems);
-    document.getElementsByClassName("gems-score")[0].innerHTML = String(gems);
-    var stars = data.users[id].stars;
-    document.getElementsByClassName("score-star")[0].innerHTML = String(stars);
-    document.getElementsByClassName("stars-score")[0].innerHTML = String(stars);
-    level = data.users[id].level;
+
+    document.body.addEventListener("keydown", function (e) {
+        if (e.keyCode === 27 && document.getElementById("myacount").style.visibility === "visible") {
+            var el = document.getElementById("myacount");
+            el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
+        }
+    });
+
+    document.getElementsByClassName("usernametitle")[0].innerHTML = "User";
+
+    var levels = {
+        0: 'level ground',
+        1: 'level water',
+        2: 'level cloud'
+    };
 
     var locked = document.getElementsByClassName("level-locked");
 
-    if(level === 0){
-        locked[level].innerHTML = "";
-        document.getElementsByClassName("level ground")[0].style.filter="grayscale(" + 0 + "%)";
+    locked[0].innerHTML = "";
+    document.getElementsByClassName("level ground")[0].style.filter = "grayscale(" + 0 + "%)";
+    locked[1].innerHTML = "";
+    document.getElementsByClassName("level water")[0].style.filter = "grayscale(" + 0 + "%)";
+    locked[2].innerHTML = "";
+    document.getElementsByClassName("level cloud")[0].style.filter = "grayscale(" + 0 + "%)";
 
-        document.getElementsByClassName(levels[level])[0].addEventListener("click", function(){
-            location.href = "ground.html?id="+id+'&user='+user;
-        });
-    }
-    if(level === 1){
-        locked[level-1].innerHTML = "";
-        document.getElementsByClassName("level ground")[0].style.filter="grayscale(" + 0 + "%)";
-        locked[level].innerHTML = "";
-        document.getElementsByClassName("level water")[0].style.filter="grayscale(" + 0 + "%)";
-
-        document.getElementsByClassName(levels[level-1])[0].addEventListener("click", function(){
-            location.href = "ground.html?id="+id+'&user='+user;
-        });
-        document.getElementsByClassName(levels[level])[0].addEventListener("click", function(){
-            location.href = "water.html?id="+id+'&user='+user;
-        });
-    }
-    if(level === 2){
-        locked[level-2].innerHTML = "";
-        document.getElementsByClassName("level ground")[0].style.filter="grayscale(" + 0 + "%)";
-        locked[level-1].innerHTML = "";
-        document.getElementsByClassName("level water")[0].style.filter="grayscale(" + 0 + "%)";
-        locked[level].innerHTML = "";
-        document.getElementsByClassName("level cloud")[0].style.filter="grayscale(" + 0 + "%)";
-
-        document.getElementsByClassName(levels[level-2])[0].addEventListener("click", function(){
-            location.href = "ground.html?id="+id+'&user='+user;
-        });
-        document.getElementsByClassName(levels[level-1])[0].addEventListener("click", function(){
-            location.href = "water.html?id="+id+'&user='+user;
-        });
-        document.getElementsByClassName(levels[level])[0].addEventListener("click", function(){
-            location.href = "sky.html?id="+id+'&user='+user;
-        });
-    }
-    // }
-});
+    document.getElementsByClassName(levels[0])[0].addEventListener("click", function () {
+        location.href = "ground.html"
+    });
+    document.getElementsByClassName(levels[1])[0].addEventListener("click", function () {
+        location.href = "water.html";
+    });
+    document.getElementsByClassName(levels[2])[0].addEventListener("click", function () {
+        location.href = "sky.html";
+    });
+}
 
 
 
